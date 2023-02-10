f72a1d2a3c0cdc524476d86b4f59f16d1b615835
Kerny <kerny14@gmail.com>
2023-02-10 14:16:32 -0500

    commit tree b67c75098ec191c8ea7844cc28bacea428c512d5

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function() {
+    let a =2000
+    let b = 100
+    return (a/b)
+}
+
+const square = (multiply) => {
+    let x = 2
+    return (x*x)
+}
+
+const add = (addition) => {
+    let a =3
+    let b=4
+    return (a+b)
+}
